17cf6a280d3125fabab8eac942b9b71399701946
Lee Fox <lee.fox@sonarsource.com>
2024-06-12 09:43:17 -0500

    Wrote the app

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+6
+app.py,1/3/13cce7fd076299c81b4986166f3d822791c9490e
+D
+templates/index.html,f/6/f69b8ce2e31684ae06f1b3e84821e07e3dfddb2f
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+from itertools import permutations
+
+app = Flask(__name__)
+
+def find_anagrams(word):
+    # Generate all possible permutations of the word
+    perms = permutations(word)
+
+    # Convert permutations to a set to remove duplicates
+    anagrams = set([''.join(p) for p in perms])
+
+    # Convert the set to a list and sort it alphabetically
+    sorted_anagrams = sorted(list(anagrams))
+
+    return sorted_anagrams
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    anagrams = []
+    if request.method == 'POST':
+        word = request.form['word']
+        anagrams = find_anagrams(word)
+    return render_template('index.html', anagrams=anagrams, len_anagrams=len(anagrams))
+
+if __name__ == '__main__':
+    app.run(debug=True)
